f1fcce0733aa8266a7367c8480393b9ef2218f0a
supertramp09 <esskay099@gmail.com>
2021-07-18 17:28:58 +0530

    Firestore listener

--- app/src/main/java/com/terranullius/yellowheart/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/terranullius/yellowheart/viewmodels/MainViewModel.kt
@@ -1,19 +1,20 @@
 package com.terranullius.yellowheart.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.FirebaseFirestore
 import com.terranullius.yellowheart.data.Initiative
 import com.terranullius.yellowheart.data.toInitiative
-import com.terranullius.yellowheart.firebase.FirebaseAuthUtils
-import com.terranullius.yellowheart.firebase.FirestoreUtils
+import com.terranullius.yellowheart.data.toInitiativeDto
+import com.terranullius.yellowheart.other.Constants
 import com.terranullius.yellowheart.utils.Result
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-
-    //TODO ADD FEATURE TO LISTEN TO REFRESH LIVE
 
     private val _initiativesFlow = MutableStateFlow<Result<List<Initiative>>>(Result.Loading)
     val initiativesFlow: StateFlow<Result<List<Initiative>>>
@@ -23,30 +24,50 @@ class MainViewModel : ViewModel() {
     val isSignedInFlow: StateFlow<Boolean>
         get() = _isSignedIn
 
+    private var initiatives: List<Initiative>? = null
+
+    private var firestoreListenerJob: Job? = null
+
     var isFirstRun = true
 
     fun onSignedIn() {
         _isSignedIn.value = true
         if (isFirstRun) {
-            refreshInitiatives()
+            if (firestoreListenerJob == null) {
+                viewModelScope.launch {
+                    firestoreListenerJob = launch {
+                        setInitiativesListener()
+                    }
+                }
+            }
             isFirstRun = false
         }
     }
 
-    private fun refreshInitiatives() {
+    private fun setInitiativesListener() {
         if (_isSignedIn.value) {
-            viewModelScope.launch {
-                _initiativesFlow.value = Result.Success(FirestoreUtils.getInitiatives().map {
-                    it.toInitiative()
-                }.sortedBy {
-                    it.order
-                })
+            val firestore = FirebaseFirestore.getInstance()
+            val initiativesCollectionRef = firestore.collection(Constants.COLLECTION_INITIATIVE)
+
+            initiativesCollectionRef.addSnapshotListener { value, error ->
+                if (error != null) {
+                    Log.d("fuck", "firestore listener error: $error")
+                }
+                value?.let {
+                    initiatives = it.documents.mapNotNull { documentSnapshot ->
+                        documentSnapshot.toInitiativeDto()
+                    }.map { initiativeDto ->
+                        initiativeDto.toInitiative()
+                    }.sortedBy { initiative ->
+                        initiative.order
+                    }
+                }
+                initiatives?.let {
+                    Log.d("fuck", "initiatives: $it")
+                    _initiativesFlow.value = Result.Success(it)
+                }
             }
         }
-    }
-
-    fun onHelpDialogClicked(){
-
     }
 
 }
